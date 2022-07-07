1bd3a1dbb1a7dd999a16291adfc02309df68f26f
faddal <faddal.ibrahim@turntabl.io>
2022-07-07 11:09:43 +0000

    link login and signup from the homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './screens/main/homepage/homepage.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginpageComponent } from './screens/auths/loginpage/loginpage.component';
 import { SignuppageComponent } from './screens/auths/signuppage/signuppage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   {path: 'login', component: LoginpageComponent},
@@ -23,7 +25,9 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
