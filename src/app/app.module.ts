81a4c64a46a1fd87b49ab7528959f786f365fd61
Manuel <emmanuel.owusu@turntabl.io>
2022-07-07 09:44:30 +0000

    feat: routing done, login and signup pages in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './screens/main/homepage/homepage.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './screens/auths/loginpage/loginpage.component';
+import { SignuppageComponent } from './screens/auths/signuppage/signuppage.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginpageComponent},
+  {path: 'signup', component: SignuppageComponent},
+  {path: 'home', component: HomepageComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+]
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SignuppageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
